6d6efcee2a0b1cd86ae8699bbeacbe8908607cee
Sparkie <78498155+Serenawaifu@users.noreply.github.com>
2025-06-03 00:40:33 +0530

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      // Add custom themes/colors here
+    },
+  },
+  plugins: [],
+};
